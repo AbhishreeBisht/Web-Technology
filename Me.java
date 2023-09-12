2bf3bb8afd7ab33569960df433c09c1b0461ac9d
AbhishreeBisht <130922936+AbhishreeBisht@users.noreply.github.com>
2023-09-12 16:30:59 +0530

    Add files via upload

---
+++ Me.java
@@ -0,0 +1,15 @@
+package abhis;
+
+public class Me {
+	public static void main(String[] args)
+	{
+		int sum=0;
+		for(int i=1;i<6;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+		
+	}
+
+}
