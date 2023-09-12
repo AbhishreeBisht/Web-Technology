8e19f087743908ba82d3e28f337c332ee3392806
AbhishreeBisht <130922936+AbhishreeBisht@users.noreply.github.com>
2023-09-12 16:33:03 +0530

    Add files via upload

---
+++ Scann.java
@@ -0,0 +1,12 @@
+package abhis;
+import java.util.Scanner;
+
+public class Scann {
+	public static void main(String[] args)
+	{
+		Scanner s1=new Scanner(System.in);
+		int x=s1.nextInt();
+		System.out.println("Input value is "+x);
+	}
+
+}
